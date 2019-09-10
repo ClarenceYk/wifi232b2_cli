877e87be4726547aab8e5a4d7d4ef0cfa15957fe
ClarenceYk <myk694204660@gmail.com>
2019-09-10 17:54:27 +0800

    Update handlers/wmode.js

--- handlers/wmode.js
+++ handlers/wmode.js
@@ -2,15 +2,15 @@
 const register = require("../configure/register")
 
 var init = function () {
-    register("AT+WMODE", {
-        action: "AT+WMODE",
-        info: "   wmode\t设置或查询 WIFI 操作模式（AP 或者 STA）\r\n"+
-              "        \t  查询：wmode\r\n"+
-              "        \t  设置：wmode=<mode>\r\n"+
-              "        \t  <mode>\r\n"+
-              "        \t   ap  无线接入点模式\r\n"+
-              "        \t   sta 无线终端模式",
-        handle: function (data, sender) {
+    let name = "AT+WMODE"
+    let handler = register.createHandler(name)
+    handler.info = "   wmode\t设置或查询 WIFI 操作模式（AP 或者 STA）\r\n"+
+                   "        \t  查询：wmode\r\n"+
+                   "        \t  设置：wmode=<mode>\r\n"+
+                   "        \t  <mode>\r\n"+
+                   "        \t   ap  无线接入点模式\r\n"+
+                   "        \t   sta 无线终端模式"
+    handler.handle = function (data, sender) {
             if (data == "+ok") {
                 console.log("WIFI232 config WMODE success")
             } else if (data.includes("+ok")) {
@@ -25,42 +25,8 @@ var init = function () {
                 console.log("[Debug] Sending:", "AT+ENTM")
             }
             sender.write("AT+ENTM\n")
-        },
-        inquire: function (sender) {
-            if (deviceReady) {
-                if (debugOn) {
-                    console.log("[Debug] Device ready")
-                    console.log("[Debug] Command sent")
-                }
-                clearInterval(intervalHandle)
-                if (debugOn) {
-                    console.log("[Debug] Sending:", this.action)
-                }
-                sender.write(this.action+"\n")
-            } else {
-                if (debugOn) {
-                    console.log("[Debug] waiting device ready...")
-                }
-            }
-        },
-        configure: function (sender, argv) {
-            if (deviceReady) {
-                if (debugOn) {
-                    console.log("[Debug] Device ready")
-                    console.log("[Debug] Command sent")
-                }
-                clearInterval(intervalHandle)
-                if (debugOn) {
-                    console.log("[Debug] Sending:", this.action+"="+argv)
-                }
-                sender.write(this.action+"="+argv+"\n")
-            } else {
-                if (debugOn) {
-                    console.log("[Debug] waiting device ready...")
-                }
-            }
-        }
-    })
+    }
+    register.register(name, handler)
 }
 
 module.exports = init
