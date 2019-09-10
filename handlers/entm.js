ee74cad32f225fbba922d30c8aa41defba9736d8
ClarenceYk <myk694204660@gmail.com>
2019-09-10 17:06:12 +0800

    Update handlers/entm.js

--- handlers/entm.js
+++ handlers/entm.js
@@ -2,39 +2,22 @@
 const register = require("../configure/register")
 
 var init = function () {
-    register("AT+ENTM", {
-        action: "AT+ENTM",
-        info: "   entm\t\t退出 AT 命令模式，返回透传模式",
-        handle: function (data, sender) {
-            if (data == "+ok") {
-                if (debugOn) {
-                    console.log("WIFI232 exit config mode")
-                }
-            } else if (data.includes("ERR")) {
-                console.log("ERROR:", data)
-            } else {
-                console.log("Unknown event:", data)
+    let name = "AT+ENTM"
+    let handle = register.createHandler(name)
+    handle.info = "   entm\t\t退出 AT 命令模式，返回透传模式"
+    handle.handle = function (data, sender) {
+        if (data == "+ok") {
+            if (debugOn) {
+                console.log("WIFI232 exit config mode")
             }
-            process.exit(0)
-        },
-        inquire: function (sender) {
-            if (deviceReady) {
-                if (debugOn) {
-                    console.log("[Debug] Device ready")
-                    console.log("[Debug] Command sent")
-                }
-                clearInterval(intervalHandle)
-                if (debugOn) {
-                    console.log("[Debug] Sending:", this.action)
-                }
-                sender.write(this.action+"\n") // "\n" is important!
-            } else {
-                if (debugOn) {
-                    console.log("[Debug] waiting device ready...")
-                }
-            }
+        } else if (data.includes("ERR")) {
+            console.log("ERROR:", data)
+        } else {
+            console.log("Unknown event:", data)
         }
-    })
+        process.exit(0)
+    }
+    register.register(name, handle)
 }
 
 module.exports = init
