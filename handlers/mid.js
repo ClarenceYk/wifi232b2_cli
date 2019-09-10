7a1f333f3551900cbba3e9593d8262ff1c30db9b
ClarenceYk <myk694204660@gmail.com>
2019-09-10 17:19:59 +0800

    Update handlers/mid.js

--- handlers/mid.js
+++ handlers/mid.js
@@ -2,41 +2,24 @@
 const register = require("../configure/register")
 
 var init = function () {
-    register("AT+MID", {
-        action: "AT+MID",
-        info: "   mid\t\t查询模块 MID",
-        handle: function (data, sender) {
-            if (data.includes("+ok")) {
-                let config = data.trim().split("=")
-                console.log("WIFI232 MID INFO:", config[1])
-            } else if (data.includes("ERR")) {
-                console.log("ERROR:", data)
-            } else {
-                console.log("Unknown event:", data)
-            }
-            if (debugOn) {
-                console.log("[Debug] Sending:", "AT+ENTM")
-            }
-            sender.write("AT+ENTM\n")
-        },
-        inquire: function (sender) {
-            if (deviceReady) {
-                if (debugOn) {
-                    console.log("[Debug] Device ready")
-                    console.log("[Debug] Command sent")
-                }
-                clearInterval(intervalHandle)
-                if (debugOn) {
-                    console.log("[Debug] Sending:", this.action)
-                }
-                sender.write(this.action+"\n")
-            } else {
-                if (debugOn) {
-                    console.log("[Debug] waiting device ready...")
-                }
-            }
+    let name = "AT+MID"
+    let handler = register.createHandler(name)
+    handler.info = "   mid\t\t查询模块 MID"
+    handler.handle = function (data, sender) {
+        if (data.includes("+ok")) {
+            let config = data.trim().split("=")
+            console.log("WIFI232 MID INFO:", config[1])
+        } else if (data.includes("ERR")) {
+            console.log("ERROR:", data)
+        } else {
+            console.log("Unknown event:", data)
         }
-    })
+        if (debugOn) {
+            console.log("[Debug] Sending:", "AT+ENTM")
+        }
+        sender.write("AT+ENTM\n")
+    }
+    register.register(name, handler)
 }
 
 module.exports = init
